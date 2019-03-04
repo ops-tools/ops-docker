3be0e85176d2bbaccfa7796860781a465547c049
Andrey Kravtsov <raidendev@gmail.com>
2019-03-04 21:24:41 +0300

    Add test

--- test/lib/main.sh
+++ test/lib/main.sh
@@ -8,7 +8,7 @@ __target="${__basepath}/../../ops-docker"
 echo -n "Should exit with error code when no config option passed"
 
 source "${__target}"; set +e
-__=$(main build)
+__="$(main build &>/dev/null)"
 
 if [[ "$?" > 0 ]]; then
   __ok
@@ -21,9 +21,22 @@ fi
 echo -n "Should exit with error code when no config file found"
 
 source "${__target}"; set +e
-__=$(main -c foo/bar build)
+__="$(main -c foo/bar build &>/dev/null)"
 
 if [[ "$?" > 0 ]]; then
+  __ok
+else
+  __fail
+fi
+
+#####
+
+echo -n "Should load config as source"
+
+source "${__target}"
+__="$(main -c test/conf/noop.cfg build 2>/dev/null)"
+
+if [[ "${__}" == "noop" ]]; then
   __ok
 else
   __fail
