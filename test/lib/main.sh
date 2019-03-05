9864d017e8677733ac01b12d9a6f88e089d06e37
Andrey Kravtsov <raidendev@gmail.com>
2019-03-05 13:51:12 +0300

    Update tests for main function

--- test/lib/main.sh
+++ test/lib/main.sh
@@ -8,7 +8,7 @@ __target="${__basepath}/../../ops-docker"
 echo -n "Should exit with error code when no config option passed"
 
 source "${__target}"; set +e
-__="$(main build &>/dev/null)"
+main build &>/dev/null
 
 if [[ "$?" > 0 ]]; then
   __ok
@@ -21,7 +21,7 @@ fi
 echo -n "Should exit with error code when no config file found"
 
 source "${__target}"; set +e
-__="$(main -c foo/bar build &>/dev/null)"
+main -c foo/bar build &>/dev/null
 
 if [[ "$?" > 0 ]]; then
   __ok
@@ -33,10 +33,23 @@ fi
 
 echo -n "Should load config as source"
 
-source "${__target}"
-__="$(main -c test/conf/noop.cfg build 2>/dev/null)"
+source "${__target}"; set +e
+main -c test/conf/docker.cfg build &>/dev/null
 
-if [[ "${__}" == "noop" ]]; then
+if [[ "${basename}" == "ops-docker-test" ]]; then
+  __ok
+else
+  __fail
+fi
+
+#####
+
+echo -n "Should set image name to basename when omitted in config"
+
+source "${__target}"; set +e
+main -c test/conf/docker.cfg build &>/dev/null
+
+if [[ "${images[0]}" == "${basename}" ]]; then
   __ok
 else
   __fail
