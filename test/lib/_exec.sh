b3ec94ade61e1d02eb694ca8ee652205cb80f716
Andrey Kravtsov <raidendev@gmail.com>
2019-03-11 23:55:29 +0300

    Add test for _exec function

--- test/lib/_exec.sh
+++ test/lib/_exec.sh
@@ -5,6 +5,35 @@ __target="${__basepath}/../../ops-docker"
 
 ######
 
+echo -n "Should call commands one by one"
+
+source "${__target}"
+
+__tmp=""
+
+__foo() {
+  __tmp+="foo"
+}
+
+__bar() {
+  __tmp+="bar"
+}
+
+__baz() {
+  __tmp+="baz"
+}
+
+_commands=("__foo" "__bar" "__baz")
+_exec
+
+if [[ "${__tmp}" == "foobarbaz" ]]; then
+  __ok
+else
+  __fail
+fi
+
+######
+
 echo -n "Should test _exec function"
 __fail
 
