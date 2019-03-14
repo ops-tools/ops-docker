5fe5275f3062fa3cb88363d66a1b3ae15c36d77e
Andrey Kravtsov <raidendev@gmail.com>
2019-03-14 08:02:50 +0300

    Add tests for _parse_uri function

--- test/lib/_parse_uri.sh
+++ test/lib/_parse_uri.sh
@@ -5,8 +5,69 @@ __target="${__basepath}/../../ops-docker"
 
 ######
 
-echo -n "Should test _parse_uri function"
-__fail
+echo "Should parse rsync-style ssh URI"
+
+### Without port
+
+echo -n "  root@localhost:/root"
+
+source "${__target}"
+
+__result="$(_parse_uri "root@localhost:/root")"
+
+if [[ "${__result}" == "root@localhost 22 /root" ]]; then
+  __ok
+else
+  __fail
+fi
+
+### With port
+
+echo -n "  root@localhost:2200/root"
+
+source "${__target}"
+
+__result="$(_parse_uri "root@localhost:2200/root")"
+
+if [[ "${__result}" == "root@localhost 2200 /root" ]]; then
+  __ok
+else
+  __fail
+fi
+
+echo
+
+######
+
+echo "Should parse URI with ssh scheme"
+
+### Without port
+
+echo -n "  ssh://root@localhost/root"
+
+source "${__target}"
+
+__result="$(_parse_uri "ssh://root@localhost/root")"
+
+if [[ "${__result}" == "root@localhost 22 /root" ]]; then
+  __ok
+else
+  __fail
+fi
+
+### With port
+
+echo -n "  ssh://root@localhost:2200/root"
+
+source "${__target}"
+
+__result="$(_parse_uri "ssh://root@localhost:2200/root")"
+
+if [[ "${__result}" == "root@localhost 2200 /root" ]]; then
+  __ok
+else
+  __fail
+fi
 
 ######
 
